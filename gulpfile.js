058c5602138afcdd7a7b023d9dc6c87217b9041d
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-08-09 17:01:13 -0400

    Add javascript linter

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,11 @@ gulp.task("spec", function() {
     .src("spec/index.html")
     .pipe(mochaPhantomJS());
 });
+
+var jshint = require('gulp-jshint');
+gulp.task("lint", function() {
+  return gulp
+    .src("./lib/**/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"));
+});
